7fc9e665847df72bee69b445c0003bae22eb2491
saitej <saitej.galipelli@gmail.com>
2022-02-22 11:33:07 +0530

    create owner Index page and controller. closes #18

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/OwnerController.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    @RequestMapping({"/","/index","/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
