ce57911ca3006c2c2f385378d548704d3ce0e904
saitej <saitej.galipelli@gmail.com>
2022-02-28 06:15:02 +0530

    Added Specialities Map Impl. Closes #35

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesServices.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesServices extends CrudService<Speciality,Long> {
+
+}
