721da3bc90b79b2c9865cf0a3c958bcde09f244e
saitej <saitej.galipelli@gmail.com>
2022-03-29 16:55:17 +0530

    added create update owner. Closes #59

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -24,4 +24,8 @@ public class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public boolean isNew() {
+        return this.id == null;
+    }
+
 }
